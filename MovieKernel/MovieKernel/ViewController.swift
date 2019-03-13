70cfc538963a8e61b354c2f783996d0e1d2b8b52
Malith Dulan Kuruwita <malithdulan7@gmail.com>
2019-03-13 12:33:45 -0700

    initial commit

---
+++ MovieKernel/MovieKernel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieKernel
+//
+//  Created by Malith Dulan Kuruwita on 3/13/19.
+//  Copyright © 2019 Malith Dulan Kuruwita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
